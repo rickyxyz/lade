a9927a3b283d32da6eabfcf182a7dc701bc2a123
Jose Jovian <josejovian55@gmail.com>
2022-07-02 15:20:07 +0900

    feat: pagination

--- components/Generic/FrameHead.js
+++ components/Generic/FrameHead.js
@@ -3,7 +3,7 @@ import clsx from "clsx";
 const FrameHead = ({ height = "small", children }) => {
 	return (
 		<div className={clsx(`fixed top-16 w-full z-20 bg-white`, [
-			height === "small" && "h-44",
+			height === "small" && "h-54",
 			height === "tall" && "h-60" ,
 		])}>
 			{ children }
