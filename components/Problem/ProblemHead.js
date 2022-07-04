ca8dd3264d37f8470eb502a05fd22ca0a02d8109
Jose Jovian <josejovian55@gmail.com>
2022-07-04 11:05:37 +0900

    feat: add time to problem head

--- components/Problem/ProblemHead.js
+++ components/Problem/ProblemHead.js
@@ -3,8 +3,9 @@ import { HiLockClosed } from "react-icons/hi";
 import { CgPlayListRemove } from "react-icons/cg";
 import Tag from "../Generic/Tag";
 import Visibility from "../Generic/Visibility";
+import { getTimeDifference } from "../Utility/date";
 
-const ProblemHead = ({ important = false, id2, owner, topic, subtopic, setting }) => {
+const ProblemHead = ({ important = false, id2, owner, topic, subtopic, time, setting }) => {
 
 	const smallFont = important ? "" : "text-xs";
 
@@ -15,18 +16,19 @@ const ProblemHead = ({ important = false, id2, owner, topic, subtopic, setting }
 					className={clsx("font-semibold cursor-pointer", smallFont)}
 					id={id2}
 					title="Click to select all"
-					onClick={() => {
+					onClick={(e) => {
 						if (window)
 							window
 								.getSelection()
 								.selectAllChildren(
-									document.getElementById(id2)
+									e.target
 								);
 					}}
 				>
 					{id2}
 				</span>{" "}
-				⦁ Posted by <b>{owner}</b>
+				⦁ Posted by <b>{owner}</b> {" "}
+				⦁ {getTimeDifference(time.createdAt)} ago
 			</div>
 			<div className="flex items-center">
 				<Visibility visibility={setting.visibility} important={important} />
