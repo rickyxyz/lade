b000e83b0f7d5ea7f25db3128eabce69012f3724
Jose Jovian <josejovian55@gmail.com>
2023-04-01 17:00:27 +0700

    chore: start anew with TS

--- components/QuillWrapper.js
+++
@@ -1,9 +0,0 @@
-import dynamic from "next/dynamic";
-import "react-quill/dist/quill.snow.css";
-
-const QuillNoSSRWrapper= dynamic(import("react-quill"), {
-    ssr: false,
-    loading: () => <></>,
-});
-
-export default QuillNoSSRWrapper;
