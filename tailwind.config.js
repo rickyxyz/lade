9528fb2c3dd76fccd02a91e674150f9faf8fa219
Jose Jovian <josejovian55@gmail.com>
2023-12-17 10:42:01 +0700

    chore: configure tailwind font sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontSize: {
+      sm: ["10px", "14px"],
+      base: ["14px", "20px"],
+      lg: ["18px", "28px"],
+      xl: ["20px", "28px"],
+    },
     extend: {},
   },
   plugins: [],
-}
+};
