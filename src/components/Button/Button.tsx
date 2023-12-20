23312bf085863e40d05228ce6212c12579b0f5d5
Jose Jovian <josejovian55@gmail.com>
2023-12-20 21:41:44 +0700

    chore: button outline + text align

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,20 @@
 import { DetailedHTMLProps, ButtonHTMLAttributes, ReactNode } from "react";
 import clsx from "clsx";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { Paragraph } from "../Paragraph";
 
 export type ButtonVariantType =
   | "primary"
   | "secondary"
   | "success"
   | "warning"
+  | "outline"
   | "danger"
   | "ghost"
   | "ghost-danger"
   | "link";
+
+export type ButtonOrderType = "first" | "middle" | "last";
 
 export interface ButtonProps
   extends DetailedHTMLProps<
@@ -19,6 +23,8 @@ export interface ButtonProps
   > {
   children: ReactNode;
   variant?: ButtonVariantType;
+  order?: ButtonOrderType;
+  alignText?: "left" | "center" | "right";
   className?: string;
   loading?: boolean;
 }
@@ -29,6 +35,8 @@ export function Button({
   className,
   loading = false,
   disabled = false,
+  alignText = "center",
+  order,
   type,
   onClick,
 }: ButtonProps) {
@@ -42,10 +50,17 @@ export function Button({
           variant === "success" && BUTTON_SUCCESS_STYLE,
           variant === "warning" && BUTTON_WARNING_STYLE,
           variant === "danger" && BUTTON_DANGER_STYLE,
+          variant === "outline" && BUTTON_OUTLINE_STYLE,
           variant === "ghost" && BUTTON_GHOST_STYLE,
           variant === "ghost-danger" && BUTTON_GHOST_DANGER_STYLE,
           variant === "link" && BUTTON_LINK_STYLE,
         ],
+        [
+          order === "first" && BUTTON_FIRST_STYLE,
+          order === "middle" && BUTTON_MIDDLE_STYLE,
+          order === "last" && BUTTON_LAST_STYLE,
+        ],
+        !order && "rounded",
         className
       )}
       type={type}
@@ -53,7 +68,16 @@ export function Button({
       disabled={disabled || loading}
     >
       {!loading ? (
-        children
+        <Paragraph
+          className={clsx(
+            alignText && BUTTON_TEXT_ALIGN_STYLE[alignText],
+            "w-full"
+          )}
+          as="p"
+          color="inherit"
+        >
+          {children}
+        </Paragraph>
       ) : (
         <AiOutlineLoading3Quarters className="my-1 justify-self-center animate-spin" />
       )}
@@ -62,10 +86,9 @@ export function Button({
 }
 
 const BUTTON_BASE_STYLE = [
-  "flex flex-row items-center justify-center px-4 py-2 h-fit",
+  "flex flex-row px-4 py-2 h-fit",
   "transition-colors duration-100 text-base",
   "disabled:cursor-not-allowed disabled:bg-opacity-50",
-  "rounded",
 ];
 
 const BUTTON_PRIMARY_STYLE = [
@@ -98,6 +121,13 @@ const BUTTON_DANGER_STYLE = [
   `active:bg-red-700`,
 ];
 
+const BUTTON_OUTLINE_STYLE = [
+  "text-teal-700 disabled:text-gray-400",
+  `hover:bg-teal-50 disabled:bg-transparent`,
+  `active:bg-teal-100 disabled:bg-transparent`,
+  `border border-secondary-2`,
+];
+
 const BUTTON_GHOST_STYLE = [
   "text-teal-700 disabled:text-gray-400",
   `hover:bg-teal-50 disabled:bg-transparent`,
@@ -115,3 +145,13 @@ const BUTTON_LINK_STYLE = [
   "hover:underline hover:text-teal-500",
   "active:underline active:text-teal-500",
 ];
+
+const BUTTON_FIRST_STYLE = ["!rounded-t"];
+const BUTTON_MIDDLE_STYLE = ["!border-t-0 !border-b-0"];
+const BUTTON_LAST_STYLE = ["!rounded-b"];
+
+const BUTTON_TEXT_ALIGN_STYLE = {
+  left: "text-left",
+  center: "text-center",
+  right: "text-right",
+};
