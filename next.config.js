b12809e404b6362d443c6fc7d0340f409c2b2b08
Jose Jovian <josejovian55@gmail.com>
2024-06-19 23:40:11 +0700

    chore: install svgr

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 // /** @type {import('next').NextConfig} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const removeImports = require("next-remove-imports")();
 module.exports = {
   ...removeImports({}),
@@ -6,5 +7,14 @@ module.exports = {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
   },
 };
